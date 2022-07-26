af249c43103ef4bf6cb78d6c5625d1e984cb3d97
pedroribeirodev <pedro.bonfim.13@gmail.com>
2022-07-26 20:57:13 -0300

    initial module 04

---
+++ modulo-04/aula01-generators-iterators/generator-iterators.js
@@ -0,0 +1,60 @@
+const assert = require('assert')
+
+function* calculation(arg1, arg2) {
+  yield arg1 * arg2
+}
+
+function* main() {
+  yield 'hello'
+  yield '-'
+  yield 'World'
+  yield* calculation(20, 10)
+}
+
+const generator = main()
+// console.log(generator.next())
+// console.log(generator.next())
+// console.log(generator.next())
+// console.log(generator.next())
+
+assert.deepStrictEqual(generator.next(), { value: 'hello', done: false })
+assert.deepStrictEqual(generator.next(), { value: '-', done: false })
+assert.deepStrictEqual(generator.next(), { value: 'World', done: false })
+assert.deepStrictEqual(generator.next(), { value: 200, done: false })
+assert.deepStrictEqual(generator.next(), { value: undefined, done: true })
+
+assert.deepStrictEqual(Array.from(main()), ['hello', '-', 'World', 200])
+assert.deepStrictEqual([...main()], ['hello', '-', 'World', 200])
+
+// --- async iterators
+const { readFile, stat, readdir } = require('fs/promises')
+
+function* promisified() {
+  yield readFile(__filename)
+  yield Promise.resolve('Hey dude')
+}
+
+async function* systemInfo() {
+  const file = await readFile(__filename)
+  yield { file: file.toString() }
+
+  const { size } = await stat(__filename)
+  yield { size }
+
+  const dir = await readdir(__dirname)
+  yield { dir }
+}
+
+// Promise.all([...promisified()]).then(results => console.log('Promisified', results))
+
+// ;(async () => {
+//   for await (const item of promisified()) {
+//     console.log('for await', item.toString())
+//   }
+// })()
+
+;(async () => {
+  for await (const item of systemInfo()) {
+    console.log('for await', item)
+  }
+})()
